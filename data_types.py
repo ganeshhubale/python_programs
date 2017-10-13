2746789d535e3cbe383bf1cb591c8521b304e332
Ganesh Hubale <ganeshhubale03@gmail.com>
2017-10-13 12:02:39 +0530

    identify data types

---
+++ data_types.py
@@ -0,0 +1,26 @@
+a = 5
+print(a, "is of type", type(a))
+
+print("\n")
+
+a = 2.0
+print(a, "is of type", type(a))
+
+print("\n")
+
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+
+print("\n")
+
+
+a = [1,2,'k','r']
+print(a, "is of type", type(a))
+
+print("\n")
+
+
+a = "rit"
+print(a, "is of type", type(a))
+
