f46e44ee917624426f9fae02136f250d2993a14b
Ganesh Hubale <ganeshhubale03@gmail.com>
2017-11-04 22:38:50 +0530

    use of class

---
+++ class_1.py
@@ -0,0 +1,18 @@
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@gmail.com'
+    def fullname(self):
+        return ("{} {}".format(self.first, self.last))
+
+emp_1 = Employee('ganesh', 'hubale', 20000)
+
+
+print(Employee.fullname(emp_1))
+print(emp_1.fullname())
+
+
+
